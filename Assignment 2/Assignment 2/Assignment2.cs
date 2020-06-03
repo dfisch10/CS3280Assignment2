7f10e0400ee905ffd790a3b5d5c9314dd6939876
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-06-02 20:04:47 -0600

    Add files via upload

---
+++ Assignment 2/Assignment 2/Assignment2.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        /// <summary>
+        /// This is my main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var namesList = new List<String>();
+
+            while (namesList.Count < 5)
+            {
+                NameInputFormatChecking(namesList);
+            }
+
+            Console.WriteLine("Would you like to sort the list of names by 'first' or 'last' name?");
+
+            SortInputDisplayer(namesList);
+
+        }
+
+        /// <summary>
+        /// Takes the input from the user as to whether they want to sort by 'first' name or 'last' name, 
+        /// and then runs the appropriate method (sortFirstName() or sortLastName()).
+        /// Otherwise it throws a message saying only 'last' and 'first' are accepted inputs.
+        /// </summary>
+        /// <param name="namesList"> List of names User inputted</param>
+        private static void SortInputDisplayer(List<string> namesList)
+        {
+            String input = Console.ReadLine();
+
+            if (input == "first")
+            {
+                sortFirstName(namesList);
+            }
+
+            else if (input == "last")
+            {
+                sortLastName(namesList);
+
+            }
+
+            else
+            {
+                Console.WriteLine("The only valid inputs are 'first' and 'last', please try again.");
+            }
+        }
+
+        /// <summary>
+        /// Checks the names the user is inputting to ensure they are not entering empty/null values, not entering duplicate names, 
+        /// and that there is a space between the first and last name.
+        /// </summary>
+        /// <param name="namesList">List of names User inputted</param>
+        private static void NameInputFormatChecking(List<string> namesList)
+        {
+            Console.Write("Please enter a first and last name(with a space in between):");
+
+            String name = Console.ReadLine();
+
+            if (namesList.Contains(name.ToLower()))
+            {
+                Console.WriteLine("You may not have duplicate names, please choose a new name.");
+            }
+
+            else if (String.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Names can not be null/empty. Please try again.");
+            }
+
+            else if (!name.Contains(" "))
+            {
+                Console.WriteLine("You must include a space between your first and last names.");
+            }
+
+            else
+            {
+                namesList.Add(name.ToLower());
+            }
+        }
+
+        /// <summary>
+        /// Takes the user inputted list, and reorganizes the names to be lastname, then first name, with a comma in between (I.E. lastname, firstname).
+        /// Then returns the new list which is sorted by last name.
+        /// </summary>
+        /// <param name="namesList">List of names User inputted</param>
+        private static void sortLastName(List<string> namesList)
+        {
+            var lastFirst = new List<String>();
+            foreach (String names in namesList)
+            {
+                var splitNames = names.Split(' ');
+
+                var firstName = splitNames[0];
+
+                var lastName = splitNames[1];
+
+                var newList = lastName + ", " + firstName;
+
+                lastFirst.Add(newList);
+            }
+
+            lastFirst.Sort();
+            foreach (String names in lastFirst)
+            {
+                Console.WriteLine(names);
+            }
+        }
+
+        /// <summary>
+        /// Takes the User inputted list and sorts it (by first name)
+        /// </summary>
+        /// <param name="namesList">List of names User inputted</param>
+        private static void sortFirstName(List<string> namesList)
+        {
+            namesList.Sort();
+
+            foreach (String names in namesList)
+            {
+                Console.WriteLine(names);
+            }
+        }
+    }
+}
